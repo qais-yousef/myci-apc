e1a8e5480cde02dfd6bac6283beade258512926e
Qais Yousef <qyousef@layalina.io>
2021-04-10 20:11:17 +0000

    copy_tools: Use the generic dev_push() step from myci
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/copy_tools.groovy
+++ vars/copy_tools.groovy
@@ -1,6 +1,6 @@
 def call() {
 	sh '''
 		adb -s ${IPADDRESS}:${PORT} shell "mkdir -p /data/myci-apc/"
-		adb -s ${IPADDRESS}:${PORT} push tools/battery_stats.sh /data/myci-apc/
 	'''
+	dev_push("tools/battery_stats.sh", "/data/myci-apc")
 }
