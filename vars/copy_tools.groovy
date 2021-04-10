923d28232ddaf3e94621dd0cb1f20f020c793d20
Qais Yousef <qyousef@layalina.io>
2021-04-10 19:46:06 +0000

    APC*.jenkinsfile: Remove hack to stash tools
    Since we use scm and jenkinsfiles, the tools directory is always checked
    out locally.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/copy_tools.groovy
+++ vars/copy_tools.groovy
@@ -1,14 +1,6 @@
 def call() {
-	node('master') {
-		cleanWs()
-		sh 'cp ../../../tools/* .'
-		sh 'ls'
-		sh 'pwd'
-		stash includes: '*', name: 'tools'
-	}
-	unstash 'tools'
 	sh '''
-	adb -s ${IPADDRESS}:${PORT} shell "mkdir -p /data/myci-apc/"
-	adb -s ${IPADDRESS}:${PORT} push battery_stats.sh /data/myci-apc/
+		adb -s ${IPADDRESS}:${PORT} shell "mkdir -p /data/myci-apc/"
+		adb -s ${IPADDRESS}:${PORT} push tools/battery_stats.sh /data/myci-apc/
 	'''
 }
